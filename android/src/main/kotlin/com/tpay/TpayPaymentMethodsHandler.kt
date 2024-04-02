4bd99574f565b00fc3b3f3702990cad98817b226
Tpay Auto Commit <devs@tpay.com>
2024-04-02 10:02:12 +0200

    Push changes

--- android/src/main/kotlin/com/tpay/TpayPaymentMethodsHandler.kt
+++
@@ -1,8 +0,0 @@
-package com.tpay
-
-import io.flutter.plugin.common.MethodChannel.Result
-import com.tpay.sdk.api.screenless.paymentMethods.*
-
-class TpayPaymentMethodsHandler(private val onResult: (GetPaymentMethodsResult) -> Unit) {
-    init { GetPaymentMethods().execute(onResult) }
-}
